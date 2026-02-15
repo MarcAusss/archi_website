6f5cb12736ef44a065dc42046c34a8fd0f6037e4
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-15 16:19:45 +0800

    asdasd

--- app/components/testimonials.tsx
+++ app/components/testimonials.tsx
@@ -1,7 +1,17 @@
+import { Roboto } from "../font";
+
+Roboto
+
 export default function TestimonialsPage() {
     return (
-        <div>
-            Enter
+        <div className="my-10">
+            <div className="flex justify-between">
+                <h1 className={`${Roboto.className} tracking-wider my-2`}>LATEST PROJECTS</h1>
+                <Link href="" className={`${Roboto.className} hover:underline transition-all tracking-wider my-2`}>VIEW ALL PROJECTS 🡕</Link>
+            </div>
+            <div className="border-t border-gray-400 pt-8 pb-14">
+                
+            </div>
         </div>
     );
 }
