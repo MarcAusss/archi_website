849b0867c9c101617fadc58421721205aa1a1185
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-15 16:19:20 +0800

    asdasd

--- app/components/expertise.tsx
+++ app/components/expertise.tsx
@@ -1,7 +1,13 @@
+import { Roboto } from "../font";
+
+
 export default function ExpertisePage() {
     return (
-        <div>
-            Enter
+        <div className="my-10">
+            <h1 className={`${Roboto.className} tracking-wider my-2`}>ABOUT US</h1>
+            <div className="border-t border-gray-400 pt-8 pb-14">
+                
+            </div>
         </div>
     );
 }
