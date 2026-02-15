b4f616707047fdeeb2603b560e058a314b7661ee
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-15 16:22:47 +0800

    asdasd

--- app/components/blogs.tsx
+++ app/components/blogs.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { Roboto } from "../font";
 
 
-export default function TestimonialsPage() {
+export default function BlogPage() {
     return (
         <div className="my-10">
             <div className="flex justify-between">
-                <h1 className={`${Roboto.className} tracking-wider my-2`}>LATEST PROJECTS</h1>
-                <Link href="" className={`${Roboto.className} hover:underline transition-all tracking-wider my-2`}>VIEW ALL PROJECTS 🡕</Link>
+                <h1 className={`${Roboto.className} tracking-wider my-2`}>INSIGHT & STORIES</h1>
+                <Link href="" className={`${Roboto.className} hover:underline transition-all tracking-wider my-2`}>VIEW ALL BLOGS🡕</Link>
             </div>
             <div className="border-t border-gray-400 pt-8 pb-14">
                 
