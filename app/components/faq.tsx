a6b7f346c1a6c2dc5d611d52f5a00b0213278e4a
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-15 16:26:40 +0800

    asdasd

--- app/components/faq.tsx
+++ app/components/faq.tsx
@@ -4,7 +4,7 @@ import { Roboto } from "../font";
 export default function FaqPage() {
     return (
         <div className="my-10">
-            <h1 className={`${Roboto.className} tracking-wider my-2`}>OUR EXPERTISE</h1>
+            <h1 className={`${Roboto.className} tracking-wider my-2`}>FAQS</h1>
             <div className="border-t border-gray-400 pt-8 pb-14">
                 
             </div>
