5d1b16019e6bd5976b96432381c8db482f3e457b
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-15 16:15:22 +0800

    asdasd

--- app/components/latest_project.tsx
+++ app/components/latest_project.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import { Roboto } from "../font";
 
 
 export default function Latest_projectPage() {
     return (
         <div className="my-10">
-            <h1 className={`${Roboto.className} tracking-wider my-2`}>LATEST PROJECTS</h1>
+            <div className="flex justify-between">
+                <h1 className={`${Roboto.className} tracking-wider my-2`}>LATEST PROJECTS</h1>
+                <Link href="" className={`${Roboto.className} hover:underline transition-all tracking-wider my-2`}>VIEW ALL PROJECTS 🡕</Link>
+            </div>
             <div className="border-t border-gray-400 pt-8 pb-14">
                 
             </div>
