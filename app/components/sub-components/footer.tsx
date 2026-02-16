a87e42643a415fe8ff1085b4ff085437bf1738d7
Marc Austin Bonagua <marcaustinbonagua@gmail.com>
2026-02-16 16:30:18 +0800

    asdasd

--- app/components/sub-components/footer.tsx
+++ app/components/sub-components/footer.tsx
@@ -1,7 +1,30 @@
+import { Roboto } from "@/app/font";
+import Link from "next/link";
+
+
+
 export default function Footer() {
     return (
         <div>
-            Enter
+            <div className="max-w-365 mx-auto mt-20 mb-4">
+                <div className={`${Roboto.className} flex justify-between`}>
+                    <p>©2026 BDARCH STUDIO. All Rights Reserved</p>
+                    <nav className="flex gap-10 uppercase font-normal">
+                        <Link href="" className={Roboto.className}>About</Link>
+                        <Link href="" className={Roboto.className}>Projects</Link>
+                        <Link href="" className={Roboto.className}>Services</Link>
+                        <Link href="" className={Roboto.className}>CONTACT US</Link>
+                    </nav>
+                    <div className="flex gap-4">
+                        <i className="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-x-twitter"></i>
+                        <i className="fa-brands fa-facebook-f"></i>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full relative h-50 ">
+                
+            </div>
         </div>
     );
 }
